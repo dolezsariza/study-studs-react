f633ee0175b2a51d44eceac0f4d2471b5c8aebd2
abelHodasz <hodabi@gmail.com>
2020-02-11 10:09:31 +0100

    created navigation for site

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Navigation from '../components/Navigation/Navigation'
 
 export default function Header() {
     return (
-        <div>
-            HEADER
-        </div>
+        <Fragment>
+            <Navigation></Navigation>
+        </Fragment>
     )
 }
