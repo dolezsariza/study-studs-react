29c02823a42070204c4c187908dc18ed2fb911dc
abelHodasz <hodabi@gmail.com>
2020-02-11 10:15:38 +0100

    Added not found page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React, { Fragment } from 'react';
-import {Provider} from "./util/Context";
-import './App.css';
+import React, { Fragment } from "react";
+import { Provider } from "./util/Context";
+import "./App.css";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/errorPages/NotFound/NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
-
 function App() {
-  return (
-    <div className="App">
-      <Provider>
-        <Content/>
-      </Provider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Provider>
+                <Content />
+            </Provider>
+        </div>
+    );
 }
 
-function Content(){
-  return(
-    <Fragment>
-      <Header/>
-      <div className ="page-container">
-        <Router>
-          <Switch>
-            <Route path = "/" exact component={Home}/>
-          </Switch>
-        </Router>
-      </div>
-      <Footer/>
-    </Fragment>
-  )
-
+function Content() {
+    return (
+        <Fragment>
+            <Header />
+            <div className="page-container">
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="*" component={NotFound} />
+                    </Switch>
+                </Router>
+            </div>
+            <Footer />
+        </Fragment>
+    );
 }
 
 export default App;
