17673ce70cccf983adcf2a9f8dc52aab700dffcb
abelHodasz <hodabi@gmail.com>
2020-02-13 13:04:55 +0100

    switched primary and secondary theme

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -2,7 +2,7 @@ import { createMuiTheme} from "@material-ui/core/styles";
 
 export const basic = createMuiTheme({
     palette: {
-        primary: {main:"#f1c40f"},
-        secondary: {main:"#2ecc71"}
+        secondary: {main:"#f1c40f"},
+        primary: {main:"#2ecc71"}
     }
 });
