a3fd545137b764edaca0aa2a866a52ed02d3cd41
abelHodasz <hodabi@gmail.com>
2020-02-13 13:03:48 +0100

    created themes.js

---
+++ src/themes/themes.js
@@ -0,0 +1,8 @@
+import { createMuiTheme} from "@material-ui/core/styles";
+
+export const basic = createMuiTheme({
+    palette: {
+        primary: {main:"#f1c40f"},
+        secondary: {main:"#2ecc71"}
+    }
+});
