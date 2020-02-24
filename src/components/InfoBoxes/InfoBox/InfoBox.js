6f5709872a5d180cb305158e4e18c7e69f88a8fb
abelHodasz <hodabi@gmail.com>
2020-02-24 12:55:44 +0100

    modal position absolute, closable

--- src/components/InfoBoxes/InfoBox/InfoBox.js
+++ src/components/InfoBoxes/InfoBox/InfoBox.js
@@ -1,24 +1,28 @@
 import SuccessInfo from "../SuccessInfo/SuccessInfo";
 import ErrorInfo from "../ErrorInfo/ErrorInfo";
-
+import "./InfoBox.css";
 import React, { Fragment } from "react";
 
 const InfoBox = props => {
     return (
         <Fragment>
-            <SuccessInfo
-                show={props.showSuccess}
-                headerText={"Success message"}
-                bodyText={"Action completed successfully"}
-            />
-            <ErrorInfo
-                show={props.showError}
-                headerText={"Error message"}
-                bodyText={props.errorMessage}
-            />
+            <div>
+                <SuccessInfo
+                    show={props.showSuccess}
+                    headerText={"Success message"}
+                    bodyText={"Action completed successfully"}
+                />
+            </div>
+            <div>
+                <ErrorInfo
+                    className="error-alert"
+                    show={props.showError}
+                    headerText={"Error message"}
+                    bodyText={props.errorMessage}
+                />
+            </div>
         </Fragment>
     );
 };
 
 export default InfoBox;
-
