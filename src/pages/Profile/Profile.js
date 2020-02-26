5057af5c5523b8bbdedf7dcb47711be146ca34cb
Mate Iszlai <iszimate@gmail.com>
2020-02-26 14:30:00 +0100

    Add profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,45 +2,91 @@ import "./Profile.css";
 import React, { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 import { getData } from "../../store/actions/repositoryActions";
+import Image from "../../components/BasicProfilePicture/Image";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
+import { useParams } from "react-router-dom";
 
 function Profile(props) {
-  const url = `/profile/${props.id}`;
+    const { username } = useParams();
+    const url = `/profile/${username}`;
 
-  useEffect(() => {
-    props.onGetData(url, ...props);
-  }, [props, url]);
+    useEffect(() => {
+        props.onGetData(url, props);
+    }, [props, url]);
 
-  return (
-    <Fragment>
-      <div className="profile-picture">
-        <img
-          src={props.profilePicture}
-          alt="../../BasicProfilePicture/defaultPicture.jpg"
-        />
-      </div>
-      <h3 className="name">
-        {props.firstName} {props.lastName} ({props.nickname})
-      </h3>
-      <div className="profile-details">
-        <div className="school">{props.school}</div>
-        <div className="City">{props.city}</div>
-        <div className="introduction">{props.introduction}</div>
-        <div className="interests">{props.interests}</div>
-      </div>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Grid container spacing={2} className="profile-content">
+                <Grid item xs className="profile-picture">
+                    <Image image={props.profilePicture} />
+                </Grid>
+                <Grid item xs>
+                    <h2 className="name">
+                        {props.firstName} {props.lastName} ({props.nickName})
+                    </h2>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <h3 className="details">Details:</h3>
+                </Grid>
+
+                <Grid container direction="column" spacing={2}>
+                    <Grid container direction="row" className="detail">
+                        <Grid item xs={2}>
+                            <h4 className="header">School:</h4>
+                        </Grid>
+                        <Grid item className="content school" xs>
+                            {props.school}
+                        </Grid>
+                    </Grid>
+                    <Grid container direction="row" className="detail">
+                        <Grid item xs={2}>
+                            <h4 className="header">City:</h4>
+                        </Grid>
+                        <Grid item className="content city" xs>
+                            {props.city}
+                        </Grid>
+                    </Grid>
+                    <Grid container direction="row" className="detail">
+                        <Grid item xs={2}>
+                            <h4 className="header">Introduction:</h4>
+                        </Grid>
+                        <Grid
+                            item
+                            className="content introduction"
+                            xs
+                            zeroMinWidth
+                        >
+                            <Typography>{props.introduction}</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container direction="row" className="detail">
+                        <Grid item xs={2}>
+                            <h4 className="header">Interests:</h4>
+                        </Grid>
+                        <Grid item className="content intersts" xs>
+                            {props.interests}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Fragment>
+    );
 }
 const mapStateToProps = state => {
-  return {
-    ...state.repository.data,
-    id: state.id
-  };
+    return {
+        ...state.repository.data,
+        userId: state.loggedIn.userId
+    };
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    onGetData: (url, props) => dispatch(getData(url, props))
-  };
+    return {
+        onGetData: (url, props) => dispatch(getData(url, props))
+    };
 };
+
+const linebrakeToHtmlConverter = line => {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
