a3619f49eeaf69e82061a32089aea886b1f70d68
Mate Iszlai <iszimate@gmail.com>
2020-02-27 16:51:16 +0100

    Add image size

--- src/components/BasicProfilePicture/Image.js
+++ src/components/BasicProfilePicture/Image.js
@@ -4,9 +4,23 @@ import defaultPicture from "../../images/defaultPicture.jpg";
 export default function Image(image) {
     console.log(image);
     if (image.image != null) {
-        return <img src={image} alt="" className="profile-picture" />;
+        return (
+            <img
+                height="150"
+                width="150"
+                src={image}
+                alt=""
+                className="profile-picture"
+            />
+        );
     }
     return (
-        <img src={defaultPicture} alt="Noimage" className="profile-picture" />
+        <img
+            height="150"
+            width="150"
+            src={defaultPicture}
+            alt="Noimage"
+            className="profile-picture"
+        />
     );
 }
