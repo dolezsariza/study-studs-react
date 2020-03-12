6d0e6d6c6c277bcbbcc57177a6627fec5c85f05e
dolezsariza <izabella.dolezsar@gmail.com>
2020-03-12 09:55:26 +0100

    Add TopicHeader.js

---
+++ src/components/GroupHeader/GroupHeader.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Card, Link, Box } from "@material-ui/core";
+import "./TopicHeader.css";
+import Date from "../Date/Date";
+
+export default function TopicHeader(props) {
+    const [raised, setRaised] = useState(false);
+
+    const toTopic = e => {
+        console.log(e.currentTarget);
+        props.history.push("/topic/" + props.id);
+    };
+
+    return (
+        <Card
+            raised={raised}
+            className="topic-header"
+            onClick={toTopic}
+            onMouseOver={() => {
+                setRaised(true);
+            }}
+            onMouseLeave={() => {
+                setRaised(false);
+            }}
+        >
+            <h4 className="topic-header-title">{props.title}</h4>
+            <p className="topic-header-message">{props.description}</p>
+            <Box className="row">
+                <div className="topic-header-username">
+                    <Link
+                        onClick={e => {
+                            e.stopPropagation();
+                            props.history.push("/profile/" + props.ownerName);
+                        }}
+                        color="secondary"
+                        className="username-link"
+                    >
+                        {props.ownerName}
+                    </Link>
+                </div>
+                <Date className="topic-header-date">{props.date}</Date>
+            </Box>
+        </Card>
+    );
+}
