ac3443027c42e6a833e82aac345b4327cdb5c3b4
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-06 19:30:52 +0200

    Change Topic to TopicHeader in Topics

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Topic from "../Topic/Topic";
+import TopicHeader from "../TopicHeader/TopicHeader";
 
 
 export default function Topics(props) {
-    console.log(props.topics);
     if (props.topics) {
         return props.topics.map(topic => (
-            <Topic
+            <TopicHeader
                 key={topic.id}
                 id={topic.id}
                 title={topic.title}
                 message={topic.message}
                 date={topic.date}
                 ownerName={topic.ownerName}
+                groupId = {topic.groupId}
             />
         ));
     } else return null;
