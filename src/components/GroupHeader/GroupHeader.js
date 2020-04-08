d939732a6fa3dcf30a7c2c2e92abec59402dbee9
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-08 11:02:25 +0200

    Add button to GroupHeader to join the group

--- src/components/GroupHeader/GroupHeader.js
+++ src/components/GroupHeader/GroupHeader.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, Link, Box } from "@material-ui/core";
+import { Card, Link, Box, Button } from "@material-ui/core";
 import "./GroupHeader.css";
 import Date from "../Date/Date";
 import history from "../../history";
@@ -10,35 +10,55 @@ export default function GroupHeader(props) {
     const toGroup = e => {
         history.push("/groups/" + props.id);
     };
+
+    const join = e => {
+        history.push({
+            pathname: "/groups/"+ props.id + "/join",
+            state: { groupName: props.title,
+                     groupId: props.id }
+          })
+    }
     return (
-        <Card
-            raised={raised}
-            className="group-header"
-            onClick={toGroup}
-            onMouseOver={() => {
-                setRaised(true);
-            }}
-            onMouseLeave={() => {
-                setRaised(false);
-            }}
-        >
-            <h4 className="group-header-title">{props.title}</h4>
-            <p className="group-header-message">{props.description}</p>
-            <Box className="row">
-                <div className="group-header-username">
-                    <Link
-                        onClick={e => {
-                            e.stopPropagation();
-                            history.push("/profile/" + props.ownerName);
-                        }}
-                        color="secondary"
-                        className="username-link"
-                    >
-                        {props.ownerName}
-                    </Link>
-                </div>
-                <Date className="group-header-date">{props.date}</Date>
-            </Box>
-        </Card>
+        
+        <div>
+            <Card
+                raised={raised}
+                className="group-header"
+                onClick={toGroup}
+                onMouseOver={() => {
+                    setRaised(true);
+                }}
+                onMouseLeave={() => {
+                    setRaised(false);
+                }}
+            >   
+                <h4 className="group-header-title">{props.title}</h4>
+                <p className="group-header-message">{props.description}</p>
+                
+                <Box className="row">
+                    <div className="group-header-username">
+                        <Link
+                            onClick={e => {
+                                e.stopPropagation();
+                                history.push("/profile/" + props.ownerName);
+                            }}
+                            color="secondary"
+                            className="username-link"
+                        >
+                            {props.ownerName}
+                        </Link>
+                    </div>
+                    <Date className="group-header-date">{props.date}</Date>
+                </Box>
+            </Card>
+            <Button 
+                variant="contained"
+                onClick={join}
+                color="secondary"
+                className="login-btn"
+                type="submit">
+                    Join group
+            </Button>
+        </div>
     );
 }
